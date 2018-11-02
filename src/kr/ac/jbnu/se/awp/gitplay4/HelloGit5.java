4a05bfb442a4cc3c21440a9752dcdd7726a5cd5b
yeon97 <cjcm1525@naver.com>
2018-11-02 16:14:28 +0900

    hello_everyone

--- src/kr/ac/jbnu/se/awp/gitplay4/HelloGit5.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/HelloGit5.java
@@ -3,5 +3,9 @@ package kr.ac.jbnu.se.awp.gitplay4;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
+		
+		System.out.println("Hello!!!");
+		System.out.println("Everyone");
+		System.out.println("AWP!!!");
 	}
 }
