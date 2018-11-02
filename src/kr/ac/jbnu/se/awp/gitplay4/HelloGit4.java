cf7caad0296d98a43aa3b4a632d361b2bde8e00d
leehyeonho <dlgusgh0812@gmail.com>
2018-11-02 16:18:47 +0900

    아뭐야이게

--- src/kr/ac/jbnu/se/awp/gitplay4/HelloGit4.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/HelloGit4.java
@@ -2,14 +2,11 @@ package kr.ac.jbnu.se.awp.gitplay4;
 
 public class HelloGit {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello Git!!");
 		System.out.println("Hi");
-=======
 		System.out.println("Hello Git!! 현호");
 		System.out.println("Hello Git!! 현호");
 		System.out.println("Hello Git!! 현호");
 		System.out.println("Hello Git!! HyeonHo");
->>>>>>> branch 'master' of https://github.com/dsryu99/kr.ac.jbnu.se.awp.gitplay4.git
 	}
 }
