d169d3cf059586b325402b3f20b8cd87cb06c21e
yeon97 <cjcm1525@naver.com>
2018-11-08 19:54:37 +0900

    Delete HelloGit.java

--- src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.jbnu.se.awp.gitplay4;
-
-public class HelloGit {
-	public static void main(String[] args) {
-		System.out.println("Hello Git!!");
-		System.out.println("Where is Pofo?");
-	}
-}
